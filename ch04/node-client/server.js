2c3845e9fd78dda4f411932151a538ec6952e054
3zzazakl <vanzekovichgr@gmail.com>
2024-02-17 03:03:23 +0200

    adding node files

---
+++ ch04/node-client/server.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const { Client } = require('pg');
+const client = new Client ({
+    host: 'my-postgres',
+    port: 5432,
+    user: 'postgres',
+    password: 'Top-Secret',
+    database: 'demo'
+})
+
+client.connect(err => {
+    if (err) {
+        console.error('connection error' + err.stack);
+    }
+    else {
+        console.log('-----------------------');
+        console.log('connected to PostgreSQL');
+        console.log('-----------------------');
+        client.query('SELECT * FROM users', (err, res) => {
+            if (err) throw err
+            console.log(res.rows);
+            client.end()
+        })
+    }
+})
